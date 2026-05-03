695f721e16e2f5d52cf53bdc050ccb9a9760ecdc
hrnr-lcl <83419665+hrnr-lcl@users.noreply.github.com>
2026-05-03 21:46:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def yukle():
             if yuzde < 60:
                 durum = "Öncelikli tekrar"
                 renk = "danger"
-            elif 60 < yuzde < 75:
+            elif 59 < yuzde < 75:
                 durum = "Orta seviye tekrar"
                 renk = "warning"
             else:
